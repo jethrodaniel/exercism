fb8dbff647a6597092c10476dfb9fd13db3949ce
jethrodaniel <jethrodaniel@users.noreply.github.com>
2018-01-04 13:04:09 -0600

    Edit ruby/grains to be more clear

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,14 +1,28 @@
 class Grains
+  @@tiles = (1..64)
+
   # Returns the number of grains on tile n
   def self.square(n)
     # Reject invalid input
-    raise ArgumentError if n < 1 || n > 64
+    raise ArgumentError unless @@tiles.include? n
 
+    # The total number of grains on tile n is
+    #
+    #   Tile (n):   1   2   3   4   5   6   ...   n
+    #   Grains  :   1   2   4   8   16  32  ...   2^(n - 1)
     2**(n - 1)
   end
 
-  # Total of all grains on the board when every tile is filled
+  # The total number of grains on the board when k tiles are filled is
+  #
+  #   Tiles filled (k): 1   2   3   4   5   6   ...   n
+  #
+  #   Number of Grains: 1   3   7   15  31  63  ...
+  #
+  #                         64
+  #                   = 1 +  ∑  2^(n - 1)
+  #                         i=1
   def self.total
-    1.upto(64).sum { |n| self.square n }
+    @@tiles.sum { |n| square n }
   end
 end
