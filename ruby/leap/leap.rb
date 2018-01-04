e53780ff091bc5e6f0de89d1239e38c74bedad8f
jethrodaniel <jethrodaniel@users.noreply.github.com>
2018-01-03 20:54:18 -0600

    Add ruby/leap

---
+++ ruby/leap/leap.rb
@@ -0,0 +1,8 @@
+class Year
+  # A leap year is every year that is evenly divisible by 4,
+  # except every year that is evenly divisible by 100,
+  # unless the year is also evenly divisible by 400
+  def self.leap?(year)
+    year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+  end
+end
