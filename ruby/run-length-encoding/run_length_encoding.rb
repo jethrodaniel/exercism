d10ba81ffcaa73b09bf8c53c39f3e7c973e4197a
jethrodaniel <jethrodaniel@users.noreply.github.com>
2018-01-10 21:48:16 -0600

    Simplify ruby/run_length_encoding with Enumerables

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,44 +1,16 @@
 class RunLengthEncoding
 
   def self.encode(input)
-    stack = []  # A stack to hold the finished characters
-
-    # Capitalize the input and remove spaces
-    input.each_char do |char|
-      if stack.last != char
-        # Just append if the stack is empty or the character is new
-        stack << char
-      else
-        stack.pop    # The last element is the same as char, so discard it
-
-        if stack.last.is_a? Numeric
-          stack << stack.pop.to_i + 1 << char  # Increment the previous number
-        else
-          stack << 2 << char                   #  Or add the first number
-        end
-      end
-    end
-
-    stack.join
+    input.each_char
+         .slice_when(&:!=)
+         .map { |x| [x.size > 1 ? x.size : '', x.first] }
+         .join
   end
 
   def self.decode(input)
-    decoded_str, numbers = '', ''
-
-    input.each_char do |char|
-      if char.match? /[[:digit:]]/
-        numbers << char   # Add the next digit to numbers
-      else
-        # The number preceding this character
-        n_times = numbers.to_i.nonzero? ? numbers.to_i : 1
-
-        numbers = ''  # Reset the numbers string
-
-        # Add the characters to the return string
-        decoded_str << char * n_times
-      end
-    end
-
-    decoded_str
+    input.each_char
+         .slice_after(/[^[[:digit:]]]/)
+         .map { |*nums, char| char * (nums.empty? ? 1 : nums.join.to_i) }
+         .join
   end
 end
