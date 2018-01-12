d02a0c25201d2e6c2bdf51d4fda12eddb133bd09
jethrodaniel <jethrodaniel@users.noreply.github.com>
2018-01-11 18:41:44 -0600

    Add csharp/sum-of-multiples

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max) =>
+        (from m in multiples
+         from n in Enumerable.Range(0, max)
+         where n % m == 0
+         select (n)).Distinct().Sum();
+}
