85e23e748ec1c458bf80eae608ca6cea8b005dd1
jethrodaniel <jethrodaniel@users.noreply.github.com>
2018-01-14 21:52:09 -0600

    Add csharp/collatz-conjecture

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,23 @@
+﻿public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number < 1)
+        {
+            throw new System.ArgumentException();
+        }
+
+        int count = 0;
+
+        while (number > 1)
+        {
+            number = number.Collatz();
+            ++count;
+        }
+
+        return count;
+    }
+
+    public static int Collatz(this int number) => 
+        (number % 2 == 0) ? number / 2 : number * 3 + 1;
+}
