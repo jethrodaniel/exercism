a964ef6660c8d8d74dda5f067bb2b0e84c11a44d
jethrodaniel <jethrodaniel@users.noreply.github.com>
2018-01-16 16:45:37 -0600

    Optimize ruby/run-length-encoding with #reduce

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,16 +1,17 @@
 class RunLengthEncoding
-
   def self.encode(input)
+    number_and_char = -> (x) { (x.size > 1 ? x.size.to_s : '') << x[0] }
+    
     input.each_char
          .slice_when(&:!=)
-         .map { |x| [x.size > 1 ? x.size : '', x.first] }
-         .join
+         .reduce('') { |encoded, x| encoded << number_and_char.call(x) }
   end
-
+  
   def self.decode(input)
+    expand = -> (seq) { seq.size == 1 ? seq.join : seq.pop * seq.join.to_i }
+
     input.each_char
-         .slice_after(/[^[[:digit:]]]/)
-         .map { |*nums, char| char * (nums.empty? ? 1 : nums.join.to_i) }
-         .join
+         .slice_after(/[^\d]/)
+         .reduce('') { |decoded, seq| decoded << expand.call(seq) }
   end
 end
