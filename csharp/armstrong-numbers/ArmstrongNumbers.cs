84d216298c87c0b915a6823cd671e5649403e52a
jethrodaniel <jethrodaniel@users.noreply.github.com>
2018-01-16 18:21:16 -0600

    Add csharp/armstrong-numbers

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        var sum = 0;
+
+        foreach (var digit in number.ToString())
+        {
+            sum += (int)Math.Pow(
+                                 int.Parse(digit.ToString()),
+                                 number.ToString().Length
+                                );
+        }                                 
+
+        return sum == number;
+    }
+}
