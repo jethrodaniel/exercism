e8577fc4310f83b2bd8799aa1b8e54533fd1fd3e
jethrodaniel <jethrodaniel@gmail.com>
2018-06-15 09:20:20 -0500

    Add cpp/hamming

---
+++ cpp/hamming/hamming.cpp
@@ -0,0 +1,18 @@
+#include <stdexcept>
+#include "hamming.h"
+
+namespace hamming {
+    int compute(std::string a, std::string b) {
+        if (a.length() != b.length()) {
+            throw std::domain_error(std::string("Strings must be the same size"));
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i <= a.length(); ++i) {
+            if (a[i] != b[i]) { ++sum; }
+        }
+
+        return sum;
+    }
+}
