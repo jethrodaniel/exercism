42b180c648221a2d60ca6f76c7525cdda6f596ba
Mark Delk <jethrodaniel@gmail.com>
2018-10-28 18:01:38 -0500

    haskell leap-year

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,4 +1,10 @@
 module LeapYear (isLeapYear) where
 
+divisible_by :: Integral a => a -> a -> Bool
+divisible_by a b = a `rem` b == 0
+
 isLeapYear :: Integer -> Bool
-isLeapYear year = error "You need to implement this function."
+isLeapYear year =
+  year `divisible_by` 4
+    && ((year `divisible_by` 400) || not (year `divisible_by` 100))
+
