938c74e58b876e9e49f856e2b4cde7828507a9ee
Mark Delk <jethrodaniel@gmail.com>
2019-06-28 00:34:08 -0400

    [rust][sum-of-multiples] cleanup

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,11 +1,5 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     (1..limit)
-        .take_while(|&n| n <= limit)
-        .filter(|n| {
-            factors
-                .iter()
-                .filter(|&&f| f > 0 as u32)
-                .any(|f| n % f == 0)
-        })
+        .filter(|n| factors.iter().any(|&f| f > 0 && n % f == 0))
         .sum()
 }
