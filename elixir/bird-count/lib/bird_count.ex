a9557a4e71e0ea4cb5712cefe88dd662ffa49b2f
Mark Delk <jethrodaniel@gmail.com>
2025-10-10 20:59:25 -0500

    elixir: bird-count, don't use stdlib

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,31 +1,20 @@
 defmodule BirdCount do
   def today([]), do: nil
-
-  def today(list) do
-    hd(list)
-  end
+  def today(list), do: hd(list)
 
   def increment_day_count([]), do: [1]
-
-  def increment_day_count(list) do
-    [today(list) + 1 | tl(list)]
-  end
+  def increment_day_count(list), do: [today(list) + 1 | tl(list)]
 
   def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([0 | _tail]), do: true
+  def has_day_without_birds?([_head | tail]), do: has_day_without_birds?(tail)
 
-  def has_day_without_birds?(list) do
-    Enum.member?(list, 0)
-  end
+  def total(list), do: total(list, 0)
+  def total([], count), do: count
+  def total([head | tail], count), do: total(tail, count + head)
 
-  def total(list) do
-    Enum.sum(list)
-  end
-
-  def busy_days([]), do: 0
-
-  def busy_days(list) do
-    list
-    |> Stream.filter(fn count -> count >= 5 end)
-    |> Enum.count()
-  end
+  def busy_days(list), do: busy_days(list, 0)
+  def busy_days([], count), do: count
+  def busy_days([head | tail], count) when head >= 5, do: busy_days(tail, count + 1)
+  def busy_days([head | tail], count) when head < 5, do: busy_days(tail, count)
 end
